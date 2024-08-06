4baa067341805ddce69b42433f2abd77f77136ae
DEFINE <privet.csgo@bk.ru>
2024-08-06 19:03:01 +0300

    edited maincpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,7 @@ using std::cout, std::endl;
 
 int main() {
   cout << "yooo" << endl;
-<<<<<<< HEAD
   Beep(60,3000);
-=======
->>>>>>> 1293aba (output - fifthMSG in main.cpp)
   fifthMSG();
   return 0;
 }
