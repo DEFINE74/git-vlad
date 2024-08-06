1cd907454fa60b7c13881141730ab766b907430e
DEFINE <privet.csgo@bk.ru>
2024-08-06 16:30:36 +0300

    added 'FIRST MSG'

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+
+#include "module.h"
+
+using std::cout, std::endl;
+
+int main() {
+  cout << "yooo" << endl;
+  firstMSG();
+  return 0;
+}
---
+++ module.cpp
@@ -0,0 +1,9 @@
+#include "module.h"
+
+#include <iostream>
+
+using std::cout, std::endl;
+
+void firstMSG() {
+  cout << "first msg" << endl;
+}
